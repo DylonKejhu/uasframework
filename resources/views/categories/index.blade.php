389abf974d847a91077387dba90fdcb7c04170f6
DylonKejhu <dylonkejhu@gmail.com>
2026-01-14 14:15:00 +0800

    Update

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,5 +1,5 @@
 <h1>Daftar Kategori</h1>
-
+<!-- Membuat kategori -->
 <a href="{{ route('categories.create') }}"><button>+ Tambah Kategori</button></a>
 
 <table border="1" cellpadding="8" cellspacing="0" width="100%" style="border-collapse: collapse;">
@@ -16,8 +16,9 @@
             <td>{{ $i + 1 }}</td>
             <td>{{ $category->name }}</td>
             <td>
+                <!-- Mengedit Kategori -->
                 <a href="{{ route('categories.edit', $category->id) }}"><button>Edit</button></a>
-
+                    <!-- Menghapus Kategori -->
                 <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline">
                     @csrf
                     @method('DELETE')
