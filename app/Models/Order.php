d0c156ad0217ba1ad3a3b4ae7ad3ce51a873f07f
DylonKejhu <dylonkejhu@gmail.com>
2026-01-19 02:36:39 +0800

    Adding Migration & Model for Orders Feature

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'supplier_name',
+        'total_cost',
+        'notes',
+    ];
+
+    protected $casts = [
+        'total_cost' => 'decimal:2',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
